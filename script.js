1f7cd609ce67154895a23c04b9733c6272cd7015
MichaelPKennedy <111917176+MichaelPKennedy@users.noreply.github.com>
2022-10-23 20:07:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 (function () {
     "use strict";
 
-    const blueplanet = "../Images/BluePlanet.png";
-    const earth = "../Images/earth.png";
-    const jupiter = "../Images/jupiter.png";
-    const moon = "../Images/Moon.png";
-    const saturn = "../Images/saturn.png"
+    const blueplanet = "./Images/BluePlanet.png";
+    const earth = "./Images/earth.png";
+    const jupiter = "./Images/jupiter.png";
+    const moon = "./Images/Moon.png";
+    const saturn = "./Images/saturn.png"
     var turnimage = document.createElement('img');
     var player = earth;
 
